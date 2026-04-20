ccd985ca7385745ff33231a6bc93cd8f6b9aab3a
Juan Pablo Dealbera <80698781+juanpid2112@users.noreply.github.com>
2026-04-20 19:31:07 -0300

    feat Proyecto

---
+++ src/main/java/com/prog4/EjemploDesdeCero/EjemploDesdeCeroApplication.java
@@ -0,0 +1,13 @@
+package com.prog4.EjemploDesdeCero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploDesdeCeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploDesdeCeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prog4/EjemploDesdeCero/EjemploDesdeCeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prog4.EjemploDesdeCero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploDesdeCeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
